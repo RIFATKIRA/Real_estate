adc8404f1f0e3065b864b8006c07288a459a8968
RIFATKIRA <redwanrifat0505@gmail.com>
2025-04-30 10:57:08 -0700

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import {Flex, Box, Text, Button} from '@chakra-ui/react';
+
+
+const banner=()=>{
+    <Flex flexWrap="wrap" justifyContent="center" alignItems="center" m="10"></Flex>
+}
+
+const Index = () => {
+  return (
+    <div>
+        <h1>Hello world</h1>
+    </div>
+  )
+}
+
+export default Index
